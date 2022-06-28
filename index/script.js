c4b3be039e9fccd7792017a24ca96fe6bfc54b78
Orhan ARI <66368354+ariorhan@users.noreply.github.com>
2022-06-28 14:28:24 +0100

    Add files via upload

---
+++ index/script.js
@@ -0,0 +1,12 @@
+   
+function changeText() {
+    var x = document.querySelector('.hero__info');
+    var y = document.querySelector('.mobile-tap-area');
+    
+    x.classList.toggle('dont-show')    
+    y.classList.toggle('dont-show')
+
+}
+
+
+
